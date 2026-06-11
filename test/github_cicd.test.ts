797fbb3a36249256f396a037cf2c7bb48023a7ee
techwithbobtech <techwithbob.tech@gmail.com>
2026-06-11 10:43:09 -0500

    Initial commit

---
+++ test/github_cicd.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as GithubCicd from '../lib/github_cicd-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/github_cicd-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new GithubCicd.GithubCicdStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
