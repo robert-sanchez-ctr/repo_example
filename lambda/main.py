0783831f79570c474bf2bd609ff482a5400d5cbd
techwithbobtech <techwithbob.tech@gmail.com>
2026-06-11 13:25:06 -0500

    Fix Lambda response format

--- lambda/main.py
+++ lambda/main.py
@@ -1,4 +1,11 @@
+import json
+
+
 def handler(event, context):
     response_body = {"message": "Hello, World!", "version": "1.0.0"}
 
-    return {"statusCode": 200, "body": response_body}
+    return {
+        "statusCode": 200,
+        "headers": {"Content-Type": "application/json"},
+        "body": json.dumps(response_body),
+    }
